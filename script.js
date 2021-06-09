182cf0988c58f847b9cdef95143f2c9a0bfdc5e4
kompotejam <simonphilippe.levasseur@gmail.com>
2021-06-09 12:22:10 +0200

    fix focus

--- script.js
+++ script.js
@@ -4,15 +4,14 @@ function compute()
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
     var interest = principal * years * rate / 100
-    var amount = interest+principal
     var year = new Date().getFullYear()+parseInt(years)
     //condition to check if the amount of principal is equal or lower to zero, if so an alert is raised
     if(principal <= 0){
-        alert("Enter a positive number")
-        principal.focus()
-        return false
+        alert("Enter a positive number");
+        document.getElementById("principal").focus();
+        return
     }
-    document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>"
+    document.getElementById("result").innerHTML="If you deposit <mark>"+principal+"</mark>,\<br\>at an interest rate of <mark>"+rate+"%</mark>\<br\>You will receive an amount of <mark>"+interest+"</mark>,\<br\>in the year <mark>"+year+"</mark>\<br\>"
 
 }
 
@@ -22,5 +21,5 @@ function compute()
 function updateRate()
 {
         var rateval = document.getElementById("rate").value
-        document.getElementById("rate_val").innerText=rateval
+        document.getElementById("rate_val").innerText=rateval+"%"
     }
